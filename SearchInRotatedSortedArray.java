6bac9f68d6dbb906488fe8d0bcca02af2a8ee963
Vansh Chawla <vanshchawla019@gmail.com>
2025-09-14 00:09:19 +0530

    Day 5 - pushing code on Github

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,34 @@
+class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check if the left half is sorted
+            if (nums[low] <= nums[mid]) {
+                // Check if target is in the sorted left half
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Otherwise, the right half must be sorted
+            else {
+                // Check if target is in the sorted right half
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
