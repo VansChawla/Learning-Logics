7992e8a5a4595fae6bbb82f263691ff5b8fbf975
Vansh Chawla <vanshchawla019@gmail.com>
2025-09-15 00:15:29 +0530

    Day-6 Push leetcode to Github

---
+++ TrappingRainWater.java
@@ -0,0 +1,31 @@
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        if(height==null || height.length==0){
+            return 0;
+        }
+
+        int n = height.length;
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
+        int trappedWater = 0;
+
+        //Calculate leftMax Array
+        leftMax[0] = height[0];
+        for(int i=1; i<n; i++){
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        }
+
+        //Calculate rightMax Array
+        rightMax[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+        }
+
+        //Calculate total trapped water
+        for(int i=0; i<n; i++){
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+    }
+}
