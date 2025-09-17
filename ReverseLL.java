332685f3f84a7525b7b59a9df43e43576b922048
Vansh Chawla <vanshchawla019@gmail.com>
2025-09-18 00:01:45 +0530

    Day-9 push

---
+++ ReverseLL.java
@@ -0,0 +1,17 @@
+class ReverseLL {
+    public ListNode reverseList(ListNode head) {
+        if(head==null){
+            return null;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
