97b2791c2719830313242585cf05dc9e525474a7
Vansh Chawla <vanshchawla019@gmail.com>
2025-10-10 23:56:36 +0530

    ..

---
+++ ValidateStackSequences.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class ValidateStackSequences {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st = new Stack<>();
+        int s = 0;
+        for(int i=0; i<pushed.length; i++){
+            st.push(pushed[i]);
+            while(!st.isEmpty() && popped[s] == st.peek()){
+                st.pop();
+                s++;
+            }
+        }
+        return st.isEmpty();
+    }
+}
