dd4b407840e05e4910e75944a5e8ffaa7d543aa8
Vansh Chawla <vanshchawla019@gmail.com>
2025-11-08 12:34:05 +0530

    daily commit

---
+++ RunningSumof1dArray.java
@@ -0,0 +1,10 @@
+public class RunningSumof1dArray {
+    public int[] runningSum(int[] nums) {
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+
+        }
+        return nums;
+    }
+}
