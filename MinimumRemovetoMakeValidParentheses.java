ba6136b7c2dbbd2ba641f0b4b51eef75f1bb2197
Vansh Chawla <vanshchawla019@gmail.com>
2025-12-03 01:19:04 +0530

    daily commit

---
+++ MinimumRemovetoMakeValidParentheses.java
@@ -0,0 +1,41 @@
+public class MinimumRemovetoMakeValidParentheses {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        int extraOpen = 0;
+
+        // Remove all invalid closing brackets
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                sb.append(ch);
+                extraOpen++;
+            } else if (ch == ')') {
+                if (extraOpen > 0) {
+                    sb.append(ch);
+                    extraOpen--;
+                } else if (extraOpen == 0) {
+                    continue;
+                }
+            } else {
+                sb.append(ch);
+            }
+        }
+
+        // Remove the extra open brackets
+        n = sb.length();
+        for (int i = n - 1; i >= 0; i--) {
+            if (extraOpen == 0) {
+                break;
+            }
+            char ch = sb.charAt(i);
+
+            if (ch == '(') {
+                sb.deleteCharAt(i);
+                extraOpen--;
+            }
+        }
+
+        return sb.toString();
+    }
+}
