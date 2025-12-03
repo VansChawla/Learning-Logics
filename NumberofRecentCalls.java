4d49af989cb4b9710e578f0628236a739a8133a6
Vansh Chawla <vanshchawla019@gmail.com>
2025-12-03 15:25:06 +0530

    add comments to the program

--- NumberofRecentCalls.java
+++ NumberofRecentCalls.java
@@ -2,20 +2,24 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class NumberofRecentCalls {
-    Queue<Integer> queue;
-
-    public RecentCounter() {
-        queue = new LinkedList<>();
-    }
-
-    public int ping(int t) {
-        queue.add(t);
-
-        while (queue.peek() < t - 3000) {
-            queue.poll();
+        Queue<Integer> queue;
+    
+        public RecentCounter() {
+            queue = new LinkedList<>();
         }
-
-        return queue.size();
-    }
-
+    
+        public int ping(int t) {
+            queue.add(t);
+    
+    
+            //A valid range is [t-3000, t]
+            //So any timestamp < t-3000 is invalid
+            while (queue.peek() < t - 3000) {
+                queue.poll();
+            }
+    
+            //The size of the queue is the number of recent calls
+            return queue.size();
+        }
+        
 }
