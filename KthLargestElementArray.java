1509e9258c403853e29f47194fe014d2abae98d8
Vansh Chawla <vanshchawla019@gmail.com>
2025-12-03 23:15:36 +0530

    doing some changes in code

--- KthLargestElementArray.java
+++ KthLargestElementArray.java
@@ -1,7 +1,20 @@
 class KthLargestElementArray {
     public int findKthLargest(int[] nums, int k) {
-        final int N = nums.length;
-        Arrays.sort(nums);
-        return nums[N - k];
+        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+
+        for(int num : nums){
+            minheap.add(num);
+
+            if(minheap.size() > k){
+                minheap.poll();
+            }
+        }
+
+        return minheap.peek();
     }
 }
+
+//Previously, I did code like this...hahahahaha
+//final int N = nums.length;
+//Arrays.sort(nums);
+//return nums[N - k];
