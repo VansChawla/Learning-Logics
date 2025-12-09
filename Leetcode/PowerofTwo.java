db46527f45012ae0b7ccdb8fe8a298ee0e1c5d92
Vansh Chawla <vanshchawla019@gmail.com>
2025-12-09 12:21:29 +0530

    daily cpmmit

---
+++ Leetcode/PowerofTwo.java
@@ -0,0 +1,11 @@
+public class PowerofTwo {
+    public boolean isPowerOfTwo(int n) {
+
+        for (int i = 0;; i++) {
+            if (Math.pow(2, i) == n)
+                return true;
+            else if (Math.pow(2, i) > n)
+                return false;
+        }
+    }
+}
