0faed347fb9a861005fdeba8270f1b1b8bcd2fd5
Vansh Chawla <vanshchawla019@gmail.com>
2025-12-17 22:15:42 +0530

    some changes

--- Python Programs/RandomNum.py
+++ Python Programs/RandomNum.py
@@ -1,5 +1,18 @@
 import random
 
-num = random.randint(1, 10)
+randNum = random.randint(1, 20)
+guess = None
 
-print(num)
+while guess != randNum:
+    
+    guess = int(input("Guess a number: "))
+    
+    if(randNum == guess):
+        print("Correct!")
+    elif(randNum < guess):
+        print("Too High")
+    else:
+        print("Too Low")
+        
+
+
