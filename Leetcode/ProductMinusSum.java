b2104856112acf41887b8c43bf80b480120ded89
Vansh Chawla <vanshchawla019@gmail.com>
2025-12-19 11:47:36 +0530

    daily commit

---
+++ Leetcode/ProductMinusSum.java
@@ -0,0 +1,14 @@
+public class ProductMinusSum {
+    public int subtractProductAndSum(int n) {
+        int sum = 0;
+        int pro = 1;
+        while (n > 0) {
+            int num = n % 10;
+            sum += num;
+            pro *= num;
+            n = n / 10;
+        }
+
+        return pro - sum;
+    }
+}
