f8a34e844e7718447e6018f303f62393904f3fb4
Vansh Chawla <vanshchawla019@gmail.com>
2025-12-29 09:58:29 +0530

    daily commit

---
+++ Leetcode/TypeofTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class TypeofTriangle {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+
+        if (nums[0] + nums[1] <= nums[2])
+            return "none";
+
+        if (nums[0] == nums[2])
+            return "equilateral";
+
+        if (nums[0] == nums[1] || nums[1] == nums[2])
+            return "isosceles";
+
+        return "scalene";
+    }
+}
