b9f2d4c329383500d23ae5262e5beffc1d8a3ef3
Vansh Chawla <vanshchawla019@gmail.com>
2026-01-09 10:23:28 +0530

    daily commit

---
+++ Leetcode/FindifDigitGameCanBeWon.java
@@ -0,0 +1,15 @@
+public class FindifDigitGameCanBeWon {
+    public boolean canAliceWin(int[] nums) {
+        int sumSingle = 0;
+        int sumDouble = 0;
+        for (int num : nums) {
+            if (num < 10) {
+                sumSingle += num;
+            } else {
+                sumDouble += num;
+            }
+        }
+
+        return sumSingle != sumDouble;
+    }
+}
