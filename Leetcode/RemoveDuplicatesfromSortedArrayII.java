6b2378995f40f59c30d89fae5ab559cf7edfadd2
Vansh Chawla <vanshchawla019@gmail.com>
2026-01-11 12:15:50 +0530

    daily commit

---
+++ Leetcode/RemoveDuplicatesfromSortedArrayII.java
@@ -0,0 +1,17 @@
+public class RemoveDuplicatesfromSortedArrayII {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int result = 2;
+
+        for (int i = 2; i < nums.length; i++) {
+
+            if (nums[i] != nums[result - 2]) {
+                nums[result] = nums[i];
+                result++;
+            }
+
+        }
+        return result;
+    }
+}
