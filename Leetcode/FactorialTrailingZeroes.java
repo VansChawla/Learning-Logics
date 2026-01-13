daa260412a31fa2cf2808b5ad42cce8979d197cb
Vansh Chawla <vanshchawla019@gmail.com>
2026-01-14 00:46:13 +0530

    daily commit

---
+++ Leetcode/FactorialTrailingZeroes.java
@@ -0,0 +1,12 @@
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int count = 0;
+
+        while (n >= 5) {
+            n /= 5;
+            count += n;
+        }
+
+        return count;
+    }
+}
