3591f36e48c22846614545829a58104f643cb5ff
Vansh Chawla <vanshchawla019@gmail.com>
2026-01-18 11:31:22 +0530

    Another method of a solved question

---
+++ Leetcode/ContainsDuplicate.java
@@ -0,0 +1,25 @@
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return true;
+            seen.add(num);
+        }
+        return false;
+    }
+}
+
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+
+//         for(int i=0; i<nums.length-1; i++){
+//             if(nums[i]==nums[i+1]){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
