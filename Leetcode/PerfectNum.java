7e4cf6cb61286ff0a06c7adb5f57394dc4cd61f0
Vansh Chawla <vanshchawla019@gmail.com>
2026-01-26 16:30:09 +0530

    daily commit

---
+++ Leetcode/PerfectNum.java
@@ -0,0 +1,12 @@
+public class PerfectNum {
+    public boolean checkPerfectNumber(int num) {
+        int count = 0;
+
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0)
+                count += i;
+        }
+
+        return count == num;
+    }
+}
