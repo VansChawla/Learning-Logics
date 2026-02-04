e91843c7457c299a8a8567764c31e051256daa9f
Vansh Chawla <vanshchawla019@gmail.com>
2026-02-04 13:07:41 +0530

    daily commit

---
+++ Leetcode/MaximumGap.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class MaximumGap {
+    public int maximumGap(int[] nums) {
+        if (nums.length <= 1)
+            return 0;
+
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(max, nums[i] - nums[i - 1]);
+        }
+
+        return max;
+    }
+}
