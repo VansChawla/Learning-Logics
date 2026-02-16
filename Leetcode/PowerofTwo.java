14f46f1649238a06a76108fd8ccbf76e0565a438
Vansh Chawla <vanshchawla019@gmail.com>
2026-02-16 19:31:13 +0530

    daily commit

--- Leetcode/PowerofTwo.java
+++ Leetcode/PowerofTwo.java
@@ -1,11 +1,17 @@
-public class PowerofTwo {
-    public boolean isPowerOfTwo(int n) {
+class Solution {
+    public boolean PowerOfTwo(int n) {
 
-        for (int i=0 ;; i++) {
-            if (Math.pow(2, i) == n)
+        //Solution one with bit manipulations
+        if(n > 0 && (n & n-1) == 0) return true;
+        return false;
+
+        //Solution two
+        for(int i=0 ;; i++){
+            if(Math.pow(2,i) == n)
                 return true;
-            else if (Math.pow(2, i) > n)
+            else if(Math.pow(2,i) > n)
                 return false;
         }
+        
     }
 }
