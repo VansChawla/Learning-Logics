3247fab9630c79cce2839deac13f72c45a509f4c
Vansh Chawla <vanshchawla019@gmail.com>
2026-03-01 12:44:42 +0530

    daily commit

---
+++ Leetcode/MaxAbsSumofSubarray.java
@@ -0,0 +1,15 @@
+public class MaxAbsSumofSubarray {
+    public int maxAbsoluteSum(int[] nums) {
+        int maxPrefix = 0;
+        int minPrefix = 0;
+        int currentPrefix = 0;
+
+        for (int num : nums) {
+            currentPrefix += num;
+            maxPrefix = Math.max(maxPrefix, currentPrefix);
+            minPrefix = Math.min(minPrefix, currentPrefix);
+        }
+
+        return maxPrefix - minPrefix;
+    }
+}
