0f626b3107b672260efac44b37a03baa3d52d368
Vansh Chawla <vanshchawla019@gmail.com>
2026-03-24 23:49:42 +0530

    daily commit

--- Leetcode/Numberof1Bits.java
+++ Leetcode/Numberof1Bits.java
@@ -1,12 +1,22 @@
-public class Numberof1Bits {
+class Solution {
     public int hammingWeight(int n) {
-        String digits = Integer.toBinaryString(n);
         int count = 0;
-        for (char digit : digits.toCharArray()) {
-            if (digit == '1') {
-                count++;
-            }
+        while(n != 0){
+            n = n & (n-1);
+            count++;
         }
         return count;
     }
 }
+// class Solution {
+//     public int hammingWeight(int n) {
+//         String digits = Integer.toBinaryString(n);
+//         int count = 0;
+//         for(char digit : digits.toCharArray()){
+//             if(digit == '1'){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
