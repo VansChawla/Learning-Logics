cb1f89115bf5c5aaac29d60771a59c989ae8f783
Vansh Chawla <vanshchawla019@gmail.com>
2026-04-14 21:36:51 +0530

    daily commit python

--- Python Programs/LabCycle1/CountDigitinNum.py
+++ Python Programs/LabCycle1/CountDigitinNum.py
@@ -1,3 +1,12 @@
-n = 78787889
-n2s = str(n)
-print(len(n2s))
+# n = 78787889
+# n2s = str(n)
+# print(len(n2s))
+
+num = int(input("Enter a number: "))
+count = 0
+
+while num > 0:
+    count += 1
+    num = num // 10
+
+print("Number of digits:", count)
