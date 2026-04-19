f8232847701098f7f251bd1583288d97d9beae9b
Vansh Chawla <vanshchawla019@gmail.com>
2026-04-19 12:40:47 +0530

    daily commit

--- Leetcode/FloodFill.java
+++ Leetcode/FloodFill.java
@@ -1,22 +1,44 @@
 public class FloodFill {
+    //New Solution
+    private void helper(int[][] image, int sr, int sc, int color, int orgColor){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc] != orgColor)
+            return;
+        
+        image[sr][sc] = color;
+
+        helper(image, sr, sc-1, color, orgColor); //left
+        helper(image, sr, sc+1, color, orgColor); //right
+        helper(image, sr-1, sc, color, orgColor); //up
+        helper(image, sr+1, sc, color, orgColor); //down
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int startingColor = image[sr][sc];
-
-        if (startingColor != color)
-            dfs(image, sr, sc, startingColor, color);
-
+        int orgColor = image[sr][sc];
+        
+        if (orgColor != color)
+            helper(image, sr, sc, color, orgColor);
         return image;
     }
 
-    private static void dfs(int image[][], int r, int c, int startingColor, int newColor) {
-        if (r < 0 || r >= image.length || c < 0 || c >= image[0].length || image[r][c] != startingColor)
-            return;
 
-        image[r][c] = newColor;
+    //Old Solution
+    // public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    //     int startingColor = image[sr][sc];
 
-        dfs(image, r + 1, c, startingColor, newColor);
-        dfs(image, r - 1, c, startingColor, newColor);
-        dfs(image, r, c + 1, startingColor, newColor);
-        dfs(image, r, c - 1, startingColor, newColor);
-    }
+    //     if (startingColor != color)
+    //         dfs(image, sr, sc, startingColor, color);
+
+    //     return image;
+    // }
+
+    // private static void dfs(int image[][], int r, int c, int startingColor, int newColor) {
+    //     if (r < 0 || r >= image.length || c < 0 || c >= image[0].length || image[r][c] != startingColor)
+    //         return;
+
+    //     image[r][c] = newColor;
+
+    //     dfs(image, r + 1, c, startingColor, newColor);
+    //     dfs(image, r - 1, c, startingColor, newColor);
+    //     dfs(image, r, c + 1, startingColor, newColor);
+    //     dfs(image, r, c - 1, startingColor, newColor);
+    // }
 }
