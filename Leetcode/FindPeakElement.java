b5867172613bf78b36d2a209facef2fade0f2187
Vansh Chawla <vanshchawla019@gmail.com>
2026-05-07 21:53:54 +0530

    daily commit

--- Leetcode/FindPeakElement.java
+++ Leetcode/FindPeakElement.java
@@ -1,19 +1,33 @@
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
         int left = 0;
-        int right = nums.length - 1;
-        int first_true_index = -1;
+        int right = nums.length-1;
 
-        while (left <= right) {
+        while(left < right){
             int mid = left + (right - left) / 2;
-            if (mid == nums.length - 1 || nums[mid] > nums[mid + 1]) {
-                first_true_index = mid;
-                right = mid - 1;
-            } else {
+            
+            if(nums[mid] < nums[mid+1])
                 left = mid + 1;
-            }
+            else 
+                right = mid;
         }
 
-        return first_true_index;
+        return left;
+
+        // int left = 0;
+        // int right = nums.length - 1;
+        // int first_true_index = -1;
+
+        // while (left <= right) {
+        //     int mid = left + (right - left) / 2;
+        //     if (mid == nums.length - 1 || nums[mid] > nums[mid + 1]) {
+        //         first_true_index = mid;
+        //         right = mid - 1;
+        //     } else {
+        //         left = mid + 1;
+        //     }
+        // }
+
+        // return first_true_index;
     }
 }
