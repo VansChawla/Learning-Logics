df42ec80333d061fc91b69e598a8090af1a34bda
Vansh Chawla <vanshchawla019@gmail.com>
2026-05-14 11:29:17 +0530

    daily commit

---
+++ Leetcode/IsomorphicStrings.java
@@ -0,0 +1,23 @@
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        int[] lastSeenS = new int[256];
+        int[] lastSeenT = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            if (lastSeenS[charS] != lastSeenT[charT]) {
+                return false;
+            }
+
+            lastSeenS[charS] = i + 1;
+            lastSeenT[charT] = i + 1;
+        }
+
+        return true;
+    }
+}
