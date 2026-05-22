d2db89bf998646b69c4f53b4da53de2faf2322c8
Vansh Chawla <vanshchawla019@gmail.com>
2026-05-22 16:20:32 +0530

    daily commit

---
+++ Leetcode/ComplementofBase10Integer.java
@@ -0,0 +1,8 @@
+public class ComplementofBase10Integer {
+    public int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+        
+        int mask = (Integer.highestOneBit(n) << 1) - 1;
+        return n ^ mask;
+    }
+}
