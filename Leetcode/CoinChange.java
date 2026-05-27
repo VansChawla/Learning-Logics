56b90dc1bc9c747f4a603054890eeb36151f99e8
Vansh Chawla <vanshchawla019@gmail.com>
2026-05-27 11:48:49 +0530

    daily commit

---
+++ Leetcode/CoinChange.java
@@ -0,0 +1,27 @@
+class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int dp[][] = new int[n+1][amount+1];
+
+        int maxVal = amount + 1;
+
+        for(int i=0; i<n+1; i++){
+            dp[i][0] = 0;
+        }
+        for(int j=1; j<amount+1; j++){
+            dp[0][j] = maxVal;
+        }
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<amount+1; j++){
+                if(coins[i-1] <= j){
+                    dp[i][j] = Math.min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][amount] == maxVal ? -1 : dp[n][amount];
+    }
+}
