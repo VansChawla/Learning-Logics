e1f257bf2ef950eaa53d89f92b3847f7a47c7fea
Vansh Chawla <vanshchawla019@gmail.com>
2026-05-30 13:02:11 +0530

    daily commit

---
+++ Leetcode/LongestCommonSubsequence.java
@@ -0,0 +1,44 @@
+class LongestCommonSubsequence {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<m+1; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return solveMem(text1, text2, n, m, dp);
+
+        // return solveRec(text1, text2, n, m);
+    }
+
+    private int solveMem(String str1, String str2, int n, int m, int dp[][]){
+        if(n == 0 || m == 0)
+            return 0;
+        
+        if(dp[n][m] != -1)
+            return dp[n][m];
+
+        if(str1.charAt(n-1) == str2.charAt(m-1)){
+            return dp[n][m] = solveMem(str1, str2, n-1, m-1, dp) + 1;
+        } else {
+            int ans1 = solveMem(str1, str2, n-1, m, dp);
+            int ans2 = solveMem(str1, str2, n, m-1, dp);
+            return dp[n][m] = Math.max(ans1, ans2);
+        }
+    }
+
+    private int solveRec(String str1, String str2, int n, int m){
+        if(n == 0 || m == 0)
+            return 0;
+        
+        if(str1.charAt(n-1) == str2.charAt(m-1)){
+            return solveRec(str1, str2, n-1, m-1) + 1;
+        } else {
+            int ans1 = solveRec(str1, str2, n-1, m);
+            int ans2 = solveRec(str1, str2, n, m-1);
+            return Math.max(ans1, ans2);
+        }
+    }
+}
