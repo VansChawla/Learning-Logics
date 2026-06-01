f3d3ea769a56d9be19a67f054f5fdca1f22ba885
Vansh Chawla <vanshchawla019@gmail.com>
2026-06-01 11:11:52 +0530

    daily commit

---
+++ Leetcode/LongestIncreasingSubsequenceLIS.java
@@ -0,0 +1,37 @@
+class LongestIncreasingSubsequenceLIS {
+    public int lengthOfLIS(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+
+        int nums2[] = new int[set.size()];
+        int i = 0;
+        for(int num : set){
+            nums2[i++] = num;
+        }
+
+        Arrays.sort(nums2);
+        return lcs(nums, nums2);
+    }
+
+    private int lcs(int arr1[], int arr2[]){
+        int n = arr1.length;
+        int m = arr2.length;
+        int dp[][] = new int[n+1][m+1];
+
+        //Intialize first row = first col = 0;
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(arr1[i-1] == arr2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
