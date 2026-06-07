579faafc5facb319c0275cf8204a8e11e21d74d3
Vansh Chawla <vanshchawla019@gmail.com>
2026-06-07 12:02:30 +0530

    refactor MinimumScoreTriangulationofPolygonMcmDp.java to use tabulation approach for minimum score triangulation

--- Leetcode/MinimumScoreTriangulationofPolygonMcmDp.java
+++ Leetcode/MinimumScoreTriangulationofPolygonMcmDp.java
@@ -1,12 +1,14 @@
-class MinimumScoreTriangulationofPolygonMcmDp {
+class Solution {
     public int minScoreTriangulation(int[] values) {
-        int n = values.length;
-        int dp[][] = new int[n][n];
+        // int n = values.length;
+        // int dp[][] = new int[n][n];
 
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return mcmMem(values, 1, n-1, dp);
+        // for(int i=0; i<n; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // return mcmMem(values, 1, n-1, dp);
+
+        return mcmTab(values);
     }
 
     //MEMOIZATION
@@ -26,5 +28,31 @@ class MinimumScoreTriangulationofPolygonMcmDp {
         }
 
         return dp[i][j] = ans;
+    }
+
+    //TABULATION
+    public int mcmTab(int[] arr){
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        //Intialization
+        for(int i=0; i<n; i++){
+            dp[i][i] = 0; //diagonal elements
+        }
+        
+        for(int len=2; len<=n-1; len++){
+            for(int i=1; i<=n-len; i++){
+                int j = i+len-1; //col
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i; k<=j-1; k++){
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1+cost2+cost3);
+                }
+            }
+        }
+
+        return dp[1][n-1];
     }
 }
