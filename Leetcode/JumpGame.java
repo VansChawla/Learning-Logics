5a146b41bf901ab38e231da91a5fea949ece3cce
Vansh Chawla <vanshchawla019@gmail.com>
2026-06-07 11:52:35 +0530

    add tabulation DP approach for Jump Game problem

--- Leetcode/JumpGame.java
+++ Leetcode/JumpGame.java
@@ -17,4 +17,25 @@ class JumpGame {
 
         return true;
     }
+
+    // Tabulation DP approach
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        boolean dp[] = new boolean[n];
+        dp[n-1] = true;
+
+        for(int i=n-2; i>=0; i--){
+            int steps = nums[i];
+            boolean ans = false;
+            for(int j=i+1; j<=i+steps && j<n; j++){
+                if(dp[j]){
+                    ans = true;
+                    break;
+                }
+            }
+            dp[i] = ans;
+        }
+
+        return dp[0];
+    }
 }
