41ab2a66854b3560356d023895a9a73b57fa681c
Vansh Chawla <vanshchawla019@gmail.com>
2026-06-19 13:41:48 +0530

    Add implementation of image rotation

---
+++ Leetcode/DSA/RotateImage.java
@@ -0,0 +1,24 @@
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        //Transpose
+        for(int i=0; i<matrix.length; i++){
+            for(int j=i; j<matrix[i].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        //Reverse every row now
+        for(int i=0; i<matrix.length; i++){
+            int k = 0, j = matrix[i].length-1;
+            while(k < j){
+                int temp = matrix[i][k];
+                matrix[i][k] = matrix[i][j];
+                matrix[i][j] = temp;
+                k++; j--;
+            }
+        }
+
+    }
+}
