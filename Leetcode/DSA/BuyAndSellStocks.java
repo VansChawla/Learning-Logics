754e1160cbf4048634fb127ac908f6ca0b9d0b31
Vansh Chawla <vanshchawla019@gmail.com>
2026-07-13 12:27:49 +0530

    Add initial implementation of maxProfit method for stock trading

--- Leetcode/DSA/BuyAndSellStocks.java
+++ Leetcode/DSA/BuyAndSellStocks.java
@@ -1,4 +1,19 @@
 public class BuyAndSellStocks {
+    //Striver's Solution
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxProfit = 0;
+
+        for(int i=1; i<prices.length; i++){
+            int cost = prices[i] - min;
+            maxProfit = Math.max(maxProfit, cost);
+            min = Math.min(min, prices[i]); //Keeping track of min price of the stock on previous days
+        }
+
+        return maxProfit;
+    }
+
+    
     public int maxProfit(int[] prices) {
         int buyPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
