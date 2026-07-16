a2faff670e6ce4f2d9801d2af0827228561419e5
Vansh Chawla <vanshchawla019@gmail.com>
2026-07-16 11:57:11 +0530

    Fix loop boundaries in rotate method and improve comments for clarity

--- Leetcode/DSA/RotateImage.java
+++ Leetcode/DSA/RotateImage.java
@@ -1,15 +1,15 @@
-public class RotateImage {
+class RotateImage {
     public void rotate(int[][] matrix) {
         //Transpose
-        for(int i=0; i<matrix.length; i++){
-            for(int j=i; j<matrix[i].length; j++){
+        for(int i=0; i<matrix.length-1; i++){
+            for(int j=i+1; j<matrix[i].length; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
 
-        //Reverse every row now
+        //Reversing each row
         for(int i=0; i<matrix.length; i++){
             int k = 0, j = matrix[i].length-1;
             while(k < j){
