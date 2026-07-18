f99b4e8775640a9e8729dc8d7105760c14ba02bd
Vansh Chawla <vanshchawla019@gmail.com>
2026-07-18 22:04:23 +0530

    Add recursive approach for right side view of binary tree

--- Leetcode/DSA/BinaryTreeRightSideView.java
+++ Leetcode/DSA/BinaryTreeRightSideView.java
@@ -15,6 +15,24 @@ class TreeNode {
 }
 
 class BinaryTreeRightSideView {
+
+    // Recursive approach to get the right side view of a binary tree
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        rightView(root, result, 0);
+        return result;
+    }
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null) return;
+
+        if(currDepth == result.size())
+            result.add(curr.val);
+
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+    }    
+
+    // Iterative approach to get the right side view of a binary tree
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) return result;
