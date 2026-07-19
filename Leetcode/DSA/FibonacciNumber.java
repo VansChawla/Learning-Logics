2d5c6f6115943f0ba8d65d4073197c74052c8efd
Vansh Chawla <vanshchawla019@gmail.com>
2026-07-19 11:39:49 +0530

    daily commit

--- Leetcode/DSA/FibonacciNumber.java
+++ Leetcode/DSA/FibonacciNumber.java
@@ -1,4 +1,27 @@
 public class FibonacciNumber {
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    public int fib(int n) {
+        if(n <= 0)
+            return 0;
+
+        if(n == 1)
+            return 1;
+        
+        int[] result = new int[n+1];
+        
+        result[0] = 0;
+        result[1] = 1;
+        for(int i=2; i<=n; i++){
+            result[i] = result[i-1] + result[i-2];
+        }
+
+        return result[n];
+    }
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
     public int fib(int n) {
         if (n <= 1)
             return n;
