53e82790232646dbabb764a8b5a92b43322a3a1b
Vansh Chawla <vanshchawla019@gmail.com>
2026-07-22 17:42:34 +0530

    Add optimal solution for finding duplicate number using Floyd's Tortoise and Hare algorithm

--- Leetcode/DSA/DuplicateNumberArray.java
+++ Leetcode/DSA/DuplicateNumberArray.java
@@ -1,5 +1,24 @@
 class DuplicateNumberArray {
 
+    // Using Floyd's Tortoise and Hare (Cycle Detection) - Optimal Solution
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+
     // Using HashMap
     public int findDuplicate(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
