43cd65be6f9a60cc8a34a378d85324cd8531177f
Vansh Chawla <vanshchawla019@gmail.com>
2026-07-24 12:51:06 +0530

    Refactor searchMatrix method for improved readability and consistency

--- Leetcode/DSA/Searcha2DMatrix.java
+++ Leetcode/DSA/Searcha2DMatrix.java
@@ -2,15 +2,17 @@ public class Searcha2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
         int rows = matrix.length;
         int cols = matrix[0].length;
+
         int left = 0;
-        int right = rows * cols - 1;
-        while (left <= right) {
-            int mid = (left + right) >>> 1;
+        int right = rows*cols-1;
+        
+        while(left <= right){
+            int mid = (left + (right - left) / 2);
             int midVal = matrix[mid / cols][mid % cols];
 
-            if (midVal == target) {
+            if(midVal == target){
                 return true;
-            } else if (midVal < target) {
+            } else if(midVal < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
