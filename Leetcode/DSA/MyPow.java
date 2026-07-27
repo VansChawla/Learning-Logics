089e28e4abf654b5eb0d2c90bbae384a03343c6d
Vansh Chawla <vanshchawla019@gmail.com>
2026-07-27 18:00:57 +0530

    Add recursive implementation of myPow method for calculating power

--- Leetcode/DSA/MyPow.java
+++ Leetcode/DSA/MyPow.java
@@ -1,4 +1,27 @@
 public class MyPow {
+
+    //Recursion Approach
+    public double myPow(double x, int n) {
+        long longN = n;
+        if (longN < 0) {
+            x = 1 / x;
+            longN = -longN;
+        }
+        return fastPow(x, longN);
+    }
+    
+    private double fastPow(double x, long n) {
+        if (n == 0) return 1.0;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+
+    
+    //Iterative Approach
     public double myPow(double x, int n) {
         if (n == 0)
             return 1.0;
