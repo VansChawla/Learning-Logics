de6be5c63ad3a446a2d4206df3ff36756aab242b
Vansh Chawla <vanshchawla019@gmail.com>
2026-08-01 17:00:12 +0530

    Add implementation of stack data structure using an array

---
+++ Leetcode/DSA/ImplementStackUsingArray.java
@@ -0,0 +1,39 @@
+public class ImplementStackUsingArray {
+    class myStack {
+    int top;
+    int st[];
+
+    public myStack(int n) {
+        top = -1;
+        st = new int[n];
+    }
+
+    public boolean isEmpty() {
+        if(top == -1) return true;
+        else return false;
+    }
+
+    public boolean isFull() {
+        if(top >= st.length-1) return true;
+        else return false;
+    }
+
+    public void push(int x) {
+        if(!isFull()){
+            top = top+1;
+            st[top] = x;
+        }
+    }
+
+    public void pop() {
+        if(!isEmpty())
+            top = top-1;
+    }
+
+    public int peek() {
+        if(isEmpty())
+            return -1;
+        return st[top];
+    }
+}
+}
