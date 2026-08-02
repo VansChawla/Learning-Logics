1701892b8ac99b162316b7991d878c5898df2b3d
Vansh Chawla <vanshchawla019@gmail.com>
2026-08-02 21:51:36 +0530

    Add implementation of Next Greater Element I using a stack and hashmap

---
+++ Leetcode/DSA/NextGreaterElementI.java
@@ -0,0 +1,28 @@
+class NextGreaterElementI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> s = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=nums2.length-1; i>=0; i--){
+
+            while(!s.isEmpty() && s.peek() <= nums2[i]){
+                s.pop();
+            }
+
+            if(!s.isEmpty()){
+                map.put(nums2[i], s.peek());
+            } else {
+                map.put(nums2[i], -1);
+            }
+
+            s.push(nums2[i]);
+        }
+
+        int[] result = new int[nums1.length];
+        for(int i=0; i<nums1.length; i++){
+            result[i] = map.get(nums1[i]);
+        }
+
+        return result;
+    }
+}
