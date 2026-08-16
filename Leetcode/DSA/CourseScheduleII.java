d9b78fe5778a3fcb77cab9c33fb2f1a0183172b1
Vansh Chawla <vanshchawla019@gmail.com>
2026-08-16 16:42:48 +0530

    Implement Kahn's algorithm for course scheduling in findOrder method

--- Leetcode/DSA/CourseScheduleII.java
+++ Leetcode/DSA/CourseScheduleII.java
@@ -4,6 +4,50 @@ import java.util.List;
 import java.util.Queue;
 
 public class CourseScheduleII {
+    //New Solution
+    public int[] findOrder(int n, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+
+        int[] indegree = new int[n];
+
+        for(int i=0;i<pre.length;i++){
+            int a = pre[i][0], b = pre[i][1]; // b->a edge
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+
+        // kahn's algorithm
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+
+        while(q.size()>0){
+            int front = q.remove();
+            ans.add(front);
+            for(int ele : adj.get(front)){
+                indegree[ele]--;
+                if(indegree[ele]==0) q.add(ele);
+            }
+        }
+
+        if (ans.size() != n) {
+            return new int[0];
+        }
+
+        int[] res = new int[ans.size()];
+        for(int i=0; i<res.length; i++){
+            res[i] = ans.get(i);
+        }
+
+        return res;
+    }
+
+    //Old Solution
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         // 1. Setup Graph and In-degree (how many prerequisites each course has)
         List<List<Integer>> adj = new ArrayList<>();
