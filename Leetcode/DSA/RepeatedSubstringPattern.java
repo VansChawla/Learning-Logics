08e909f3cc1b63c14626ae04824d59686122d4fe
Vansh Chawla <vanshchawla019@gmail.com>
2026-08-17 23:05:56 +0530

    daily commit

---
+++ Leetcode/DSA/RepeatedSubstringPattern.java
@@ -0,0 +1,8 @@
+class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String chopped = doubled.substring(1, doubled.length() - 1);
+        
+        return chopped.contains(s);
+    }
+}
