b4e2bdb265c2a02c9f5124e2f83c26068e092ffc
Vansh Chawla <vanshchawla019@gmail.com>
2026-08-21 21:38:04 +0530

    Add MinInsertionStepstoMakeStringPalindrome class to calculate minimum insertions for palindrome formation

---
+++ Leetcode/DSA/MinInsertionStepstoMakeStringPalindrome.java
@@ -0,0 +1,23 @@
+class MinInsertionStepstoMakeStringPalindrome {
+    public int minInsertions(String s) {
+        return s.length() - longestPalindromeSubseq(s);
+    }
+    public int longestPalindromeSubseq(String s) {
+        String s_reversed = new StringBuilder(s).reverse().toString();
+        int n = s.length();
+        int dp[][] = new int[n+1][n+1];
+
+        //Tabulation DP
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(s.charAt(i-1) == s_reversed.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+
+        return dp[n][n];
+    }
+}
