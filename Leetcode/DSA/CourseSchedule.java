7edabe20094fe8eba22f6e2d2157d24eb3a520db
Vansh Chawla <vanshchawla019@gmail.com>
2026-08-28 15:05:50 +0530

    Add DFS implementation in CourseSchedule class to determine if all courses can be finished

--- Leetcode/DSA/CourseSchedule.java
+++ Leetcode/DSA/CourseSchedule.java
@@ -1,4 +1,37 @@
 class CourseSchedule {
+    //DFS
+    private static boolean ans;
+    public void dfs(int i, List<List<Integer>> adj, boolean[] vis, boolean[] path){
+        vis[i] = true;
+        path[i] = true;
+        for(int ele : adj.get(i)){
+            if(path[ele] == true){
+                ans = false;
+                return;
+            }
+            if(vis[ele] == false) dfs(ele, adj, vis, path);
+        }
+        path[i] = false;
+    }
+    public boolean canFinish(int n, int[][] pre) {
+        ans = true;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        for(int i=0; i<pre.length; i++){
+            int a = pre[i][0], b = pre[i][1];
+            adj.get(b).add(a);
+        }
+
+        boolean[] vis = new boolean[n];
+        boolean[] path = new boolean[n];
+        for(int i=0; i<n; i++){
+            if(vis[i] == false) dfs(i, adj, vis, path);
+        }
+
+        return ans;
+    }
+
+    //BFS
     public boolean canFinish(int n, int[][] pre) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=1;i<=n;i++) adj.add(new ArrayList<>());
