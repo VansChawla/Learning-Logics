5e0b605fa9833e4075ee7afae8336d7b1bcece15
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-02 11:26:37 +0530

    Add ExcelSheetColumnTitle class to convert numbers to Excel column titles

---
+++ Leetcode/DSA/ExcelSheetColumnTitle.java
@@ -0,0 +1,14 @@
+class ExcelSheetColumnTitle {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+
+        while(columnNumber > 0){
+            columnNumber--;
+            int remainder = columnNumber % 26;
+            result.append((char) (remainder + 'A'));
+            columnNumber /= 26;
+        }
+
+        return result.reverse().toString();
+    }
+}
