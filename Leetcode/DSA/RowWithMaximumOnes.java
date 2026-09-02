d13f955dad446f09cd10eb5da3c2bf2f6f22cbab
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-02 19:07:11 +0530

    Add RowWithMaximumOnes class to find the row with the maximum number of ones in a matrix

---
+++ Leetcode/DSA/RowWithMaximumOnes.java
@@ -0,0 +1,21 @@
+class RowWithMaximumOnes {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int[] ans = new int[2];
+        int oneCount = 0;
+        int maxCount = Integer.MIN_VALUE;
+
+        for(int i=0; i<mat.length; i++){
+            oneCount = 0;
+            for(int j=0; j<mat[0].length; j++){
+                if(mat[i][j] == 1) oneCount++;
+            }
+            if(oneCount > maxCount && oneCount != maxCount){
+                ans[0] = i;
+            }
+            maxCount = Math.max(maxCount, oneCount);
+            ans[1] = maxCount;
+        }
+
+        return ans;
+    }
+}
