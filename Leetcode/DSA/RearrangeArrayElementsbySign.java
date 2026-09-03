bf56575a80ee409a2ddef8488517f732f9de265b
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-03 21:45:11 +0530

    Add RearrangeArrayElementsbySign class to rearrange array elements by sign

---
+++ Leetcode/DSA/RearrangeArrayElementsbySign.java
@@ -0,0 +1,19 @@
+class RearrangeArrayElementsbySign {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int i=0, j=1;
+
+        for (int k = 0; k < n; k++) {
+            if (nums[k] > 0) {
+                result[i] = nums[k];
+                i += 2;
+            } else {
+                result[j] = nums[k];
+                j += 2;
+            }
+        }
+
+        return result;
+    }
+}
