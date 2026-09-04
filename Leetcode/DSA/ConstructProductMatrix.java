a55ffb62715b22c708112f1913f44871841507b2
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-05 00:56:07 +0530

    Add ConstructProductMatrix class to compute product matrix from input grid

---
+++ Leetcode/DSA/ConstructProductMatrix.java
@@ -0,0 +1,26 @@
+class ConstructProductMatrix {
+    public int[][] constructProductMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] ans = new int[n][m];
+        int MOD = 12345;
+
+        long currentProduct = 1; 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans[i][j] = (int) currentProduct;
+                currentProduct = (currentProduct * grid[i][j]) % MOD;
+            }
+        }
+
+        currentProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                ans[i][j] = (int) ((ans[i][j] * currentProduct) % MOD);
+                currentProduct = (currentProduct * grid[i][j]) % MOD;
+            }
+        }
+
+        return ans;
+    }
+}
