eaacfe3acf35e274512e9398350ece102edad184
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-04 12:39:52 +0530

    Add Easy Approach for Spiral Order Traversal in SpiralMatrix class

--- Leetcode/DSA/SpiralMatrix.java
+++ Leetcode/DSA/SpiralMatrix.java
@@ -1,4 +1,43 @@
 class SpiralMatrix {
+    // Easy and Understandable Approach
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int fr = 0;
+        int lr = m-1;
+        int fc = 0;
+        int lc = n-1;
+        List<Integer> list = new ArrayList<>();
+
+        while(fr <= lr && fc <= lc){
+            for(int i=fc; i<=lc; i++){
+                list.add(matrix[fr][i]);
+            }
+            fr++;
+            if(fr>lr || fc>lc) break;
+
+            for(int i=fr; i<=lr; i++){
+                list.add(matrix[i][lc]);
+            }
+            lc--;
+            if(fr>lr || fc>lc) break;
+
+            for(int i=lc; i>=fc; i--){
+                list.add(matrix[lr][i]);
+            }
+            lr--;
+            if(fr>lr || fc>lc) break;
+            
+            for(int i=lr; i>=fr; i--){
+                list.add(matrix[i][fc]);
+            }
+            fc++;
+        }
+
+        return list;
+    }
+
+    // Directional Approach
     public List<Integer> spiralOrder(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
