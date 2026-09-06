caf9cad37e839e63809d49294f2d777a62698857
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-06 22:21:31 +0530

    Add sliding window implementation for finding maximum average subarray

--- Leetcode/DSA/MaxAvgSubarrayI.java
+++ Leetcode/DSA/MaxAvgSubarrayI.java
@@ -1,4 +1,24 @@
 public class MaxAvgSubarrayI {
+    // Sliding Window
+    public double findMaxAverage(int[] nums, int k) {
+        double sum = 0.00;
+        double max = -Double.MAX_VALUE;
+
+        int n = nums.length;
+        int l = 0;
+        for(int r=0; r<n; r++){
+            sum += nums[r];
+            if(r-l+1 == k){
+                max = Math.max(max, sum);
+                sum -= nums[l];
+                l++;
+            }
+        }
+
+        return max/k;
+    }
+
+    // Old Solution
     public double findMaxAverage(int[] nums, int k) {
         int sum = 0;
         for (int i = 0; i < k; i++) {
