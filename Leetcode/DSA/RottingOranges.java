eadf4741ab4d9dd790abc48aac3f32baa6e9659d
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-06 11:44:48 +0530

    Implement BFS algorithm to calculate minutes until all oranges rot

--- Leetcode/DSA/RottingOranges.java
+++ Leetcode/DSA/RottingOranges.java
@@ -1,4 +1,72 @@
 public class RottingOranges {
+    // BFS
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        int freshCount = 0;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 2)
+                    q.add(new int[]{i, j});
+                else if(grid[i][j] == 1)
+                    freshCount++;
+            }
+        }
+
+        if(freshCount == 0) return 0;
+
+        int minutes = 0;
+
+        while(!q.isEmpty()){
+            boolean anyConversion = false;
+            int size = q.size();
+
+            for(int i=0; i<size; i++){
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+
+                if(r > 0 && grid[r-1][c] == 1){ //Up
+                    grid[r-1][c] = 2;
+                    freshCount--;
+                    q.add(new int[]{r-1, c});
+                    anyConversion = true;
+                }
+
+                if(c > 0 && grid[r][c-1] == 1){ //Left
+                    grid[r][c-1] = 2;
+                    freshCount--;
+                    q.add(new int[]{r, c-1});
+                    anyConversion = true;
+
+                }
+                
+                if(r+1 < m && grid[r+1][c] == 1){ //Down
+                    grid[r+1][c] = 2;
+                    freshCount--;
+                    q.add(new int[]{r+1, c});
+                    anyConversion = true;
+                }
+
+                if(c+1 < n && grid[r][c+1] == 1){ //Right
+                    grid[r][c+1] = 2;
+                    freshCount--;
+                    q.add(new int[]{r, c+1});
+                    anyConversion = true;
+                }
+            }
+
+            if(anyConversion)
+                minutes++;
+        }
+
+        return freshCount == 0 ? minutes : -1;
+    }
+
+    //  Old 
     public int orangesRotting(int[][] grid) {
         if (grid == null || grid.length == 0)
             return 0;
