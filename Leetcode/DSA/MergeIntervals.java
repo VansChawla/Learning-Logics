cd88b97942936d0532e018b23ea8bb802a5444ef
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-09 17:42:58 +0530

    Add empty line at the end of MinCosttoConnectAllPoints.java for consistency

--- Leetcode/DSA/MergeIntervals.java
+++ Leetcode/DSA/MergeIntervals.java
@@ -5,6 +5,35 @@ import java.util.List;
 
 class MergeIntervals {
 
+    //Easy to understand solution, Time Complexity: O(nlogn) + O(n) = O(nlogn)
+    public int[][] merge(int[][] arr) {
+        if (arr == null || arr.length <= 1)
+            return arr;
+
+        //Sort intervals based on the start element
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+        int start1 = arr[0][0];
+        int end1 = arr[0][1];
+
+        for(int i=1; i<arr.length; i++){
+            int start2 = arr[i][0];
+            int end2 = arr[i][1];
+            if(end1 >= start2){
+                end1 = Math.max(end1, end2);
+            } else {
+                merged.add(new int[]{start1, end1});
+
+                start1 = start2;
+                end1 = end2;
+            }
+        }
+        merged.add(new int[]{start1, end1});
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
     // Time Complexity: O(nlogn) + O(n) = O(nlogn), Striver Solution
     public int[][] merge(int[][] arr) {
         if (arr == null || arr.length <= 1)
