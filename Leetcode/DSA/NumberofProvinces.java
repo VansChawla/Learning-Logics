3291f094e6ea0818c2910a7b4ccb7004c32528a3
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-13 11:10:33 +0530

    Add Disjoint Set Union implementation to NumberofProvinces class for efficient province counting

--- Leetcode/DSA/NumberofProvinces.java
+++ Leetcode/DSA/NumberofProvinces.java
@@ -1,4 +1,55 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class NumberofProvinces {
+    // DSU (Disjoint Set Union)
+    static int[] parent;
+    static int[] size;
+    
+    public int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    public void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a!=b){
+            if(size[a] > size[b]){
+                parent[b] = a;
+                size[a] += size[b];
+            } else {
+                parent[a] = b;
+                size[b] += size[a];
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] adj) {
+       int n = adj.length;
+        parent = new int[n+1];
+        size = new int[n+1];
+        for(int i=1; i<=n; i++){
+            parent[i] = i;
+            size[i] = i;
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i!=j && adj[i][j]==1) union(i+1, j+1); //connecting group leaders of i+1 & j+1
+            }
+        }
+
+        int count = 0;
+        for(int i=1; i<=n ;i++){
+            if(parent[i] == i) count++;
+        }
+
+        return count;
+    }
+    
+
+    //DFS/BFS
     public int findCircleNum(int[][] adj) {
         int n = adj.length;
         int count = 0;
