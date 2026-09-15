7c8a899c58f456cd383bb820cf534b77672d8e0e
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-15 11:03:16 +0530

    Refactor DeleteMidOfStack class to include a recursive approach for deleting the middle element from a stack

--- Leetcode/DSA/DeleteMidOfStack.java
+++ Leetcode/DSA/DeleteMidOfStack.java
@@ -1,4 +1,24 @@
 class DeleteMidOfStack {
+    
+    // Recursive approach
+    public void deleteMid(Stack<Integer> s) {
+        if(s.isEmpty()) return;
+        solRec(s, 0, s.size());
+    }
+    private void solRec(Stack<Integer> s, int count, int size){
+        if(count == size / 2){
+            s.pop();
+            return;
+        }
+        
+        int num = s.pop();
+        
+        solRec(s, count+1, size);
+        
+        s.push(num);
+    }
+
+    // Iterative approach
     public void deleteMid(Stack<Integer> s) {
         if(s.isEmpty())
             return;
