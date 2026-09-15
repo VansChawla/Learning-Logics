c4f9e0b700f2a59a2c559219070a46aff4c4ebb4
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-15 10:36:20 +0530

    Add Stack-based implementation for reversing a string in ReverseString class

--- Leetcode/DSA/ReverseString.java
+++ Leetcode/DSA/ReverseString.java
@@ -1,4 +1,18 @@
 class ReverseString {
+    // Using Stack
+    public void reverseString(char[] s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s){
+            stack.push(ch);
+        }
+
+        int i = 0;
+        while(!stack.isEmpty()){
+            s[i++] = stack.pop();
+        }
+    }
+
+    // Using Two Pointers
     public void reverseString(char[] s) {
         int st = 0;
         int ed = s.length-1;
