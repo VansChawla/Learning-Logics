ffdc3bf44178261804e79c7885d2540718d4e019
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-22 10:41:26 +0530

    Add SumOfAllOddLengthSubarrays class to calculate the sum of all odd-length subarrays

---
+++ Leetcode/DSA/SumOfAllOddLengthSubarrays.java
@@ -0,0 +1,15 @@
+class SumOfAllOddLengthSubarrays {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int totalSum = 0;
+        for(int i=0; i<arr.length; i++){
+            int sum = 0;
+            for(int j=i; j<arr.length; j++){
+                sum += arr[j];
+                if((j-i+1) % 2 != 0){
+                    totalSum += sum;
+                }
+            }
+        }
+        return totalSum;
+    }
+}
