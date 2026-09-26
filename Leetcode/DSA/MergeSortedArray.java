57b09e1d2c8b2e700c82d852b207b8d46e3cc980
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-26 14:13:47 +0530

    Add Two-Pointer Method for merging arrays in MergeSortedArray class

--- Leetcode/DSA/MergeSortedArray.java
+++ Leetcode/DSA/MergeSortedArray.java
@@ -2,6 +2,31 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
 
+    // Function to merge the arrays using the Two-Pointer Method
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
     // Function to merge the arrays using the Gap Method (Shell Sort variant)
     private void swapIfGreater(int[] nums1, int[] nums2, int ind1, int ind2) {
         if (nums1[ind1] > nums2[ind2]) {
