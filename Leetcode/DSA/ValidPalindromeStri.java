d55814de3ecca85bea9daf55bca801827a9d8867
Vansh Chawla <vanshchawla019@gmail.com>
2026-09-26 15:17:23 +0530

    Add ValidPalindrome class with two solutions for palindrome checking

---
+++ Leetcode/DSA/ValidPalindromeStri.java
@@ -0,0 +1,44 @@
+public class ValidPalindromeStri {
+    //Solution 1: Two pointer approach
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i < j){
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+                continue;
+            }
+
+            if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+                continue;
+            }
+
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    //Solution 2: Clean string and check
+    public boolean isPalindrome(String s) {
+        String cs = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); // clean string
+        int n = cs.length();
+
+        if (cs.length() == 1)
+            return true;
+
+        for (int i = 0; i < n; i++) {
+            if (cs.charAt(i) != cs.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
